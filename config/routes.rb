7ab093599e19bc2825cf091ed411b281108ea1e3
EC2 Default User <ec2-user@ip-172-31-36-184.ap-northeast-1.compute.internal>
2024-08-01 17:39:11 +0000

    コメント機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   resources :books do
     resource :favorite, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update]
 end
